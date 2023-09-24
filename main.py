13e990f31298c48d53bce6ceb4537f60a7240a78
stnganyani <85542837+coolhands-stn@users.noreply.github.com>
2023-09-25 01:36:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
-import streamlit as streamlit
+import streamlit as st
 import tensorflow as tf
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-import pickle
+import pickle 
+import numpy as np
 
-st.title(" Shona words prediction ")
 
-words = st.text_input("Input your text")
+st.title(" Next Word Predictor For Shona ")
+
+words = st.text_input("Type Text")
 
 st.write(words)
 
-# Load the tokenizer
-with open('tokenizer.pkl', 'rb') as tokenizer_file:
+with open('tokenizer.pickle','rb') as tokenizer_file:
     tokenizer = pickle.load(tokenizer_file)
 
 
 encoded_data = tokenizer.texts_to_sequences([words])[0]
-sequences = pad_sequences([encoded_data], maxlen=6, padding='pre')
+sequences = pad_sequences([encoded_data], maxlen=5, padding='pre')
 
 # Load the model
-model = tf.keras.models.load_model("nameofmodel")
+model = tf.keras.models.load_model("best_model1.h5")
 predicted_probs = model.predict(sequences)
 predicted = np.argmax(predicted_probs, axis=-1)
     
-    # Convert the predicted word index to a word
-    output_word = ""
-    for word, index in tokenizer.word_index.items():
-        if index == predicted:
-            output_word = word
-            break
+ # Convert the predicted word index to a word
+output_word = ""
+for word, index in tokenizer.word_index.items():
+    if index == predicted:
+        output_word = word
+        break
 
 st.write('your next word is', output_word)
-
