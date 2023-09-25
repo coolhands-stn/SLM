c060cc172c48fea8410c5e827a3e9e0049573a3a
stnganyani <85542837+coolhands-stn@users.noreply.github.com>
2023-09-25 02:17:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,19 @@ from keras.preprocessing.sequence import pad_sequences
 import pickle 
 import numpy as np
 
+st.title("Stany Ganyani R204442S")
+st.title("Next Word Predictor For Shona")
 
-st.title(" Next Word Predictor For Shona ")
+words = st.text_input("Nyora Apa")
 
-words = st.text_input("Type Text")
-
-st.write(words)
+# st.write(words)
 
 with open('tokenizer.pickle','rb') as tokenizer_file:
     tokenizer = pickle.load(tokenizer_file)
 
-
+# Convert Text to numerical data
 encoded_data = tokenizer.texts_to_sequences([words])[0]
+# Create sequences
 sequences = pad_sequences([encoded_data], maxlen=5, padding='pre')
 
 # Load the model
@@ -24,11 +25,11 @@ model = tf.keras.models.load_model("first_model.h5")
 predicted_probs = model.predict(sequences)
 predicted = np.argmax(predicted_probs, axis=-1)
     
- # Convert the predicted word index to a word
-output_word = ""
+# Convert the predicted word index to a word
+next_word = ""
 for word, index in tokenizer.word_index.items():
     if index == predicted:
         output_word = word
         break
 
-st.write('your next word is', output_word)
+st.write('your next word is', next_word)
